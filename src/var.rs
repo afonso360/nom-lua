4f0f772b32a324597af8df4f945badcb0c528e2e
Afonso Bordado <afonsobordado@az8.co>
2017-04-24 17:05:34 +0000

    Add varlist

--- src/var.rs
+++ src/var.rs
@@ -23,58 +23,15 @@ use ast::ASTNode;
 use ast::ASTNode::*;
 use exp::{parse_prefixexp, parse_exp};
 use name::parse_name;
-
-// TODO: Should work, Needs tests
-//named!(pub parse_varlist<ASTNode>, map!(map!(
-//            map!(do_parse!(
-//                   a: parse_var
-//                >> b: many0!(preceded!(ws!(tag!(",")), parse_var))
-//                >> ((a,b))
-//            ), |(a, mut b): (_, Vec < ASTNode >) | { b.insert(0, a); b }),
-//Box::new), ASTNode::VarList));
-
-   /// indicates a correct parsing, the first field containing the rest of the unparsed data, the second field contains the parsed data
-  //Done(I,O),
-  /// contains a Err, an enum that can indicate an error code, a position in the input, and a pointer to another error, making a list of errors in the parsing tree
-  //Error(Err<I,E>),
-  /// Incomplete contains a Needed, an enum than can represent a known quantity of input data, or unknown
-  //Incomplete(Needed)
-
 use nom::IResult;
-/*
-pub fn parse_var(input: &[u8]) -> IResult<&[u8], ASTNode> {
-    /// The function tries to parse a `Name`, if it succeds
-    /// we attempt to match that with either [ `exp` ] or . `Name`
-    /// if parsing of the Name fails we try the `prefixexp`
 
-    let var = match parse_var(input) {
-        IResult::Done(un, var) => Ok((un, var)),
-        IResult::Incomplete(need) => { return IResult::Incomplete(need) },
-        IResult::Error(err) => Err(err) ,
-    };
-
-    if let Ok(name_t) = var {
-        match opt!(name_t.0, delimited!(ws!(tag!("[")), ws!(parse_exp), tag!("]"))) {
-            IResult::Done(un, var) => {
-                return IResult::Done(un, astb!(VarPrefixed, name_t.1, var.unwrap()))
-            },
-            IResult::Incomplete(need) => { return IResult::Incomplete(need) },
-            _ => {},
-        };
-
-        match opt!(name_t.0, preceded!(ws!(tag!(".")), parse_name)) {
-            IResult::Done(un, res) => {
-                return IResult::Done(un, astb!(VarPrefixed, name_t.1, res.unwrap()))
-            },
-            IResult::Incomplete(need) => { return IResult::Incomplete(need) },
-            _ => {},
-        };
-
-    }
-
-    unimplemented!();
-}
-*/
+named!(pub parse_varlist<ASTNode>, map!(map!(
+            map!(do_parse!(
+                   a: parse_var
+                >> b: many0!(preceded!(ws!(tag!(",")), parse_var))
+                >> ((a,b))
+            ), |(a, mut b): (_, Vec < ASTNode >) | { b.insert(0, a); b }),
+Box::new), ASTNode::VarList));
 
 named!(pub parse_var<ASTNode>, alt!(
     map!(do_parse!(
@@ -122,4 +79,17 @@ mod tests {
               astb!(VarListAccess,
                     astb!(PrefixExp, astb!(Var, ast!(Name, "ayy".into()))),
                     ast!(Name, "zxc".into())));
+
+    ast_test!(parse_varlist_1, parse_varlist, "xcz", astb!(VarList, vec![
+        astb!(Var, ast!(Name, "xcz".into()))
+    ]));
+    ast_test!(parse_varlist_2, parse_varlist, "xcz , mcx", astb!(VarList, vec![
+        astb!(Var, ast!(Name, "xcz".into())),
+        astb!(Var, ast!(Name, "mcx".into()))
+    ]));
+    ast_test!(parse_varlist_3, parse_varlist, "lak , k, jd3", astb!(VarList, vec![
+        astb!(Var, ast!(Name, "lak".into())),
+        astb!(Var, ast!(Name, "k".into())),
+        astb!(Var, ast!(Name, "jd3".into()))
+    ]));
 }
